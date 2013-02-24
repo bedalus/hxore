c5b7836d3f3c80057c0e3eed72a01658c212c83e
David Savage <bedalus@ubuntu.lan>
2013-02-24 11:02:34 +0000

    build commit

--- build.sh
+++ build.sh
@@ -1,22 +1,33 @@
     echo -e "Making HOX+ zImage\n"
-    export PATH=$PATH:/opt/toolchain_linaro2/bin/
+    export PATH=$PATH:/opt/toolchain/bin/
     export ARCH=arm
     export SUBARCH=arm
     export CROSS_COMPILE=arm-eabi-
-    make -j3 
-## > /home/dave/android/history.txt
 
-    cp ./arch/arm/boot/zImage ../kernelinjector.oxp_stock/zImage.new/
-    
-    find ./drivers -type f -name '*.ko' -exec cp -f {} ../kernelinjector.oxp_stock/structure.new/modules \;
-    find ./arch -type f -name '*.ko' -exec cp -f {} ../kernelinjector.oxp_stock/structure.new/modules \;
+# delete everything
+rm -fR kernelinjector.oxp/structure.new/modules/*
+rm -f kernelinjector.oxp/zImage.new/zImage
 
-    ls -l ./arch/arm/boot/zImage 
+cd kernelinjector.oxp
+sh extract
+cd ..
 
-    cd ../kernelinjector.oxp_stock/
+# make
+make -j3
 
-    sh ./compile
-    mv ./bootimg.out/boot.img ./bootimg.out/boot$(date +%Y%m%d%H%M).img
+# copy modules
+find drivers -type f -name '*.ko' -exec cp -f {} kernelinjector.oxp/structure.new/modules \;
+find arch -type f -name '*.ko' -exec cp -f {} kernelinjector.oxp/structure.new/modules \;
+find fs -type f -name '*.ko' -exec cp -f {} kernelinjector.oxp/structure.new/modules \;
 
-    echo -e "Check zImage timestamp is correct\n"
-##
+# copy zImage
+cp -f arch/arm/boot/zImage kernelinjector.oxp/zImage.new/zImage
+
+cd kernelinjector.oxp
+sh compile
+cd ..
+
+#gzip kernelinjector.oxp/structure.new/modules/* kernelinjector.oxp/structure.new/modules/mods.tar.gz
+
+# ready for further editing
+
