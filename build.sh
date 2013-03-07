3842b09e1778127962928e361d9e9c6761e5ba7c
David Savage <bedalus@gmail.com>
2013-03-07 20:35:57 +0000

    build fixes

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
     echo -e "Making HOX+ zImage\n"
-    export PATH=$PATH:/opt/toolchain/bin/
+    export PATH=$PATH:/opt/toolchain2/bin/
     export ARCH=arm
     export SUBARCH=arm
-    export CROSS_COMPILE=arm-linux-androideabi-
+    export CROSS_COMPILE=arm-zen-linux-gnueabi-
 
 # delete everything
-rm -fR kernelinjector.oxp/structure.new/modules/*
+#rm -fR kernelinjector.oxp/structure.new/modules/*
 rm -f kernelinjector.oxp/zImage.new/zImage
 
 # cd kernelinjector.oxp
@@ -16,9 +16,7 @@ rm -f kernelinjector.oxp/zImage.new/zImage
 make -j3
 
 # copy modules
-find drivers -type f -name '*.ko' -exec cp -f {} kernelinjector.oxp/structure.new/modules \;
-find arch -type f -name '*.ko' -exec cp -f {} kernelinjector.oxp/structure.new/modules \;
-find fs -type f -name '*.ko' -exec cp -f {} kernelinjector.oxp/structure.new/modules \;
+find ./ -type f -name '*.ko' -exec cp -f {} kernelinjector.oxp/ramdisk/modules \;
 
 # copy zImage
 cp -f arch/arm/boot/zImage kernelinjector.oxp/zImage.new/zImage
