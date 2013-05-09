dce537a15936b8b07865fd50e73b01c23467ba5a
David Savage <bedalus@gmail.com>
2013-05-09 16:51:40 +0100

    switch to BFS CPU scheduler

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -28,6 +28,7 @@
 #include <linux/cpu.h>
 #include <linux/completion.h>
 #include <linux/mutex.h>
+#include <linux/sched.h>
 #include <linux/syscore_ops.h>
 #include <linux/pm_qos_params.h>
 
@@ -1491,7 +1492,15 @@ int __cpufreq_driver_target(struct cpufreq_policy *policy,
 	trace_cpu_scale(policy->cpu, policy->cur, POWER_CPU_SCALE_START);
 	if (cpu_online(policy->cpu) && cpufreq_driver->target)
 		retval = cpufreq_driver->target(policy, target_freq, relation);
+
 	trace_cpu_scale(policy->cpu, target_freq, POWER_CPU_SCALE_DONE);
+
+	if (likely(retval != -EINVAL)) {	//BFS
+		if (target_freq == policy->max)
+			cpu_nonscaling(policy->cpu);
+		else
+			cpu_scaling(policy->cpu);
+	}
 
 	return retval;
 }
